e1c060dea879bed1894f0397c4e32ceb9e884aaa
Kelsey Keppel <kelkeppel@mail.mvnu.edu>
2025-12-18 13:35:58 -0500

    Dialogue system added

--- Code/tales-of-sheepishburg/npc.gd
+++ Code/tales-of-sheepishburg/npc.gd
@@ -1,4 +1,11 @@
 extends Interactable
+const Balloon = preload("res://Dialogue/balloon.tscn")
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
 
 func _on_interacted(body):
 	$AudioStreamPlayer3D.play()
+	
+	var balloon: Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_resource, dialogue_start)
