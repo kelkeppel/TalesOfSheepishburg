774fd319861dc86855c1bca997459bd042fced24
Kelsey Keppel <kelkeppel@mail.mvnu.edu>
2026-03-20 20:21:18 -0400

    Update camera_spring_arm.gd

--- Code/tales-of-sheepishburg/camera_spring_arm.gd
+++ Code/tales-of-sheepishburg/camera_spring_arm.gd
@@ -5,6 +5,7 @@ extends SpringArm3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotation.y -= event.relative.x * mouse_sensitivity
